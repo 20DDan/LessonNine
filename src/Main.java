2245d441dc9671eb953f621f365a9adec99c957d
Dan Ratiu <dan.ratiu85@gmail.com>
2022-03-17 21:23:46 +0200

    Messing around 9000

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This Is My First JAva Project ... NOT!");
         System.out.println("Hello World!");
+
+        String Mihai;
+        String Ionel;
+        String Ghita;
+
+        System.out.println(" Mihai &"  + " Ionel &" + " Gusti" + " toti au mers la piata");
+
+
     }
 
 }
